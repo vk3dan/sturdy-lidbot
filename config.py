d725cf39f2420199a82c291980bfd3ff94158585
vk3dan <dangaz@gmail.com>
2021-03-18 10:11:06 +1100

    update config file for more detail

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-# Can be multiple prefixes, like this: ("!", "?")
+# To use this bot you need to set up the bot in here,
+# You need to decide the prefix you want to use,
+# and you need your Token and Application ID from
+# the discord page where you manage your apps and bots.
+# You need your User ID which you can get from the
+# context menu on your name in discord under Copy ID.
+
 BOT_PREFIX = ("YOUR_BOT_PREFIX_HERE")
 TOKEN = "YOUR_TOKEN_HERE"
 APPLICATION_ID = "YOUR_APPLICATION_ID"
 OWNERS = [123456789, 987654321]
 BLACKLIST = []
- # Default cogs that I have created for the template
+ # Default cogs that I use in the bot at the moment
 STARTUP_COGS = [
     "cogs.general", "cogs.help", "cogs.owner", "cogs.ham",
 ]
